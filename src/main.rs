236045c7460fcb155abbf83484f6d9bcaf571d3e
João Ribeiro <joao.rosado.ribeiro@gmail.com>
2025-12-18 12:23:30 +0000

    exit: was found

--- src/main.rs
+++ src/main.rs
@@ -4,13 +4,26 @@ use std::io::{self, Write};
 fn main() {
     let mut input = String::new();
 
+    let valid_commands = ["exit"];
+
     loop {
         print!("$ ");
         io::stdout().flush().unwrap();
 
         io::stdin().read_line(&mut input).unwrap();
 
-        println!("{}: command not found", input.trim());
+        let command = valid_commands
+            .iter()
+            .find(|command| command.to_string() == input.trim());
+
+        match command {
+            Some(c) => {
+                if c == &"exit" {
+                    break;
+                }
+            }
+            None => println!("{}: command not found", input.trim()),
+        };
 
         input.clear();
     }
